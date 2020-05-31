616e6213df5868577efc4d005a8c60a625a2175a
Marlon Domingues <marlon_domingues@outlook.com>
2020-05-31 20:59:23 -0300

    whyle infinito no início

--- Main.c
+++ Main.c
@@ -20,6 +20,10 @@ int main()
     t_embarcacao submarine = {'S', "Submarine", 3, 2};
 
     t_embarcacao *embarcacoes[5] = {&battleship, &carier, &destroyer, &patrol_boat, &submarine};
+    while (true){
+    
+    system("cls");
+
     imprime_capa(); //imprime a tela inicial com o desenho do barco
 
     scanf("%i", &opicao); //opição do modo de jogo
@@ -33,118 +37,45 @@ int main()
         inicializar_jogo(player1);
         imprimir_tela(player1);
         imprimir_instrucoes(embarcacoes);
-        break;
-    }
-    do
-    {
-        //Obter input
-        embarcacao = setar_tipo_embarcacao();
-        //fazer um f para verificar dsponibilidade de embarcacao, se quant > 0
-        orientacao = setar_direcao_embarcacao();
-        setar_coordenadas_de_entrada(&coord_x, &coord_y);
-
-        //Alocar embarcacao
-        alocar_embarcacao(coord_x, coord_y, orientacao, player1, embarcacoes[embarcacao]);
-        Atualizar_contagem_embarcacao(embarcacoes[embarcacao]);
-        // Reimprimir tela
-        imprimir_tela(player1);
-        imprimir_instrucoes(embarcacoes);
-    } while (verificar_embarcacoes_disponiveis(embarcacoes));
-
-    /*while(battleship.max_quantidade || carier.max_quantidade || destroyer.max_quantidade || patrol_boat.max_quantidade || submarine.max_quantidade)
-    {
-
-        scanf(" %[^\n]c ", &tag);
-        tag = toupper(tag);
-        Atualizar_contagem_embarcacao(tag, embarcacoes);
-        
-    }
-
-    for (i = 0; i < 5; i++)
-    {
-        printf("%c - %s - %d - %d \n", embarcacoes[i]->tag, embarcacoes[i]->nome, embarcacoes[i]->tamanho, embarcacoes[i]->max_quantidade);
-    }
-
-    /*while (nport < MAX_PORT || ncour < MAX_COUR || ntorp < MAX_TORP || nhidro < MAX_HIDRO)
-    {
-
-        printf("\n\n                   BATALHA NAVAL\n\n");
-
-        imprime_tela(player1);
-
-        while (nport < maxport || ncour < maxcour || ntorp < maxtorp || nhidro < maxhidro)
+        do
         {
+            //Obter input
+            embarcacao = setar_tipo_embarcacao();
+            //fazer um f para verificar dsponibilidade de embarcacao, se quant > 0
+            orientacao = setar_direcao_embarcacao();
+            setar_coordenadas_de_entrada(&coord_x, &coord_y);
 
-            //aloca todas as embarcações
-            imprimir_instrucoes(nport, ncour, ntorp, nhidro);
+            //Alocar embarcacao
+            alocar_embarcacao(coord_x, coord_y, orientacao, player1, embarcacoes[embarcacao]);
+            Atualizar_contagem_embarcacao(embarcacoes[embarcacao]);
+            // Reimprimir tela
+            imprimir_tela(player1);
+            imprimir_instrucoes(embarcacoes);
+        } while (verificar_embarcacoes_disponiveis(embarcacoes));
 
-            tipo = setar_tipo_embarcacao();
-            setar_coordenadas_de_entrada(&coord1, &coord2);
-            direcao = setar_direcao_embarcacao();
+        break;
+    case 2:
 
-            switch (tipo)
-            {
-            case (1):
-                if (nport < maxport)
-                    nport = nport + aloca_barco(porta_aviao, direcao, coord1, coord2, &player1); //porta avioes
+        printf("\n\n           FUNCAO AINDA NAO IMPLEMENTADA.");
+        system("pause");
 
-                else
-                {
-                    printf("ja foram utilizadas todas os porta avioes.\n");
-                    getch();
-                }
-
-                break;
-            case (2):
-                if (ncour < maxcour)
-                    ncour = ncour + aloca_barco(couracado, direcao, coord1, coord2, &player1); //couraçado
-
-                else
-                {
-                    printf("ja foram utilizadas todas os couracados.\n");
-                    getch();
-                }
-
-                break;
-            case (3):
-                if (ntorp < maxtorp)
-                    ntorp = ntorp + aloca_barco(torpedeiro, direcao, coord1, coord2, &player1); //torpedeiro
-
-                else
-                {
-                    printf("ja foram utilizadas todas os torpedeiros.\n");
-                    getch();
-                }
-
-                break;
-            case (4):
-                if (nhidro < maxhidro)
-                    nhidro = nhidro + aloca_barco(hidroaviao, direcao, coord1, coord2, &player1); //hidro avião
-
-                else
-                {
-                    printf("ja foram utilizadas todas os hidroavioes.\n");
-                    getch();
-                }
-
-                break;
-
-            default:
-                printf("Embarcacao inexistente.");
-                getch();
-                break;
-            }
-            system("cls");
-            printf("\n\n                   BATALHA NAVAL\n\n");
-            imprime_tela(player1);
-        }
-
-        getch();
         break;
     
-    default:
+    case 3:
+
+        printf("\n\n           FUNCAO AINDA NAO IMPLEMENTADA.");
+        system("pause");
         break;
+
+    default:
+
+        printf("\n\n           OPCAO INVALIDA.");
+        system("pause");
+        break;
+
     }
-*/
+
+    }
+
     return 0;
 }
